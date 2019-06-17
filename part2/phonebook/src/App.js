7a4c7983689e8f7885ee0b746d1e9f29482eedfe
cvssea <42877793+cvssea@users.noreply.github.com>
2019-06-17 22:58:31 +0300

    axios, json-server & useEffect for phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const emptyPerson = { name: '', number: '' };
   const [newPerson, setNewPerson] = useState({ ...emptyPerson });
   const [filtered, setFiltered] = useState([...persons]);
+
+  // fetch persons
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   // event handlers
   const addPerson = (e) => {
@@ -42,8 +45,8 @@ const App = () => {
   const handleFilter = (e) => {
     const { value } = e.target;
     setFiltered([
-      ...persons.filter((person) =>
-        person.name.toLowerCase().includes(value.toLowerCase())
+      ...persons.filter((p) =>
+        p.name.toLowerCase().includes(value.toLowerCase())
       ),
     ]);
   };
@@ -59,7 +62,7 @@ const App = () => {
         newPerson={newPerson}
       />
       <h3>Numbers</h3>
-      <Persons persons={filtered} />
+      <Persons persons={filtered.length ? filtered : persons} />
     </div>
   );
 };
